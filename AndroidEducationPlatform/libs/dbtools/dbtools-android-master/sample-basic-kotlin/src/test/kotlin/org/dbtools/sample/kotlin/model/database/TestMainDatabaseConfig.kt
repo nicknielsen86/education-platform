514702f051aad2a12ee109a074cfeefb2f2b9433
nnielsen <nick@plus11.com>
2016-05-23 06:53:17 -0600

    Removing db tools folder. added library via gradle.

--- AndroidEducationPlatform/libs/dbtools/dbtools-android-master/sample-basic-kotlin/src/test/kotlin/org/dbtools/sample/kotlin/model/database/TestMainDatabaseConfig.kt
+++
@@ -1,7 +0,0 @@
-package org.dbtools.sample.kotlin.model.database
-
-import org.dbtools.android.domain.config.BuildEnv
-import org.dbtools.android.domain.config.TestDatabaseConfig
-
-class TestMainDatabaseConfig(databaseFilename: String) :
-        TestDatabaseConfig(DatabaseManagerConst.MAIN_DATABASE_NAME, BuildEnv.GRADLE, databaseFilename, DatabaseManager.mainTablesVersion, DatabaseManager.mainViewsVersion)
