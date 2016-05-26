ba6e9ab0be245777364c6b583b366056220a56ec
Nick Nielsen <nicknielsen86@gmail.com>
2016-05-25 21:45:54 -0600

    adding start of course setup.

---
+++ AndroidEducationPlatform/app/src/main/java/com/educationplatform/androideducationplatform/ActivityCourse.java
@@ -0,0 +1,13 @@
+package com.educationplatform.androideducationplatform;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class ActivityCourse extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_Course);
+    }
+}
